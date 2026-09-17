10d4604fd5f98d66056c3d1121a471471144a90b
BlueberryJelly <rodionklevlin0602@gmail.com>
2026-09-17 20:03:30 +0400

    feat: first subtask of the second task was completed

---
+++ lab_1/script.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+echo "Task 2"
+
+echo "======================================"
+echo "Subtask 0"
+mkdir -p data_analysis
+ls -d data_analysis
+echo "Subtask 0 was completed"
+echo "======================================"
+
+
+echo "======================================"
+echo "Subtask 1"
+if [ -f data_analysis/research_themes.csv ]; then
+    rm -f data_analysis/research_themes.txt
+fi
+cut -d',' -f3 < faculty_5.csv \
+| tr -s '+' '\n' \
+| sed 's/^[[:space:]]*//; s/[[:space:]]*$//' \
+| sort | uniq | sort \
+| awk '{ print $0; count++ } END { print count+0 }' >> data_analysis/research_themes.txt
+awk '{ print $0 }' < data_analysis/research_themes.txt
+echo "Subtask 1 was completed"
+echo "======================================"
+
+
+echo "======================================"
+echo "Subtask 2"
+echo "Subtask 2 was completed"
+echo "======================================"
+
+
+echo "======================================"
+echo "Subtask 3"
+echo "Subtask 3 was completed"
+echo "======================================"
+
+
+echo "======================================"
+echo "Subtask 4"
+echo "Subtask 4 was completed"
+echo "======================================"

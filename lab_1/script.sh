442d2d34732baeabdea3e6bedd18c69f8411381f
BlueberryJelly <rodionklevlin0602@gmail.com>
2026-09-17 20:32:56 +0400

    fix: subtask 1

--- lab_1/script.sh
+++ lab_1/script.sh
@@ -12,15 +12,18 @@ echo "======================================"
 
 echo "======================================"
 echo "Subtask 1"
-if [ -f data_analysis/research_themes.csv ]; then
-    rm -f data_analysis/research_themes.txt
+if [ -f 'data_analysis/research_themes.csv' ]; then
+    rm -f data_analysis/research_themes.csv
 fi
 cut -d',' -f3 < faculty_5.csv \
 | tr -s '+' '\n' \
 | sed 's/^[[:space:]]*//; s/[[:space:]]*$//' \
-| sort | uniq | sort \
-| awk '{ print $0; count++ } END { print count+0 }' >> data_analysis/research_themes.txt
-awk '{ print $0 }' < data_analysis/research_themes.txt
+| sort | uniq -c | sort -k2,2\
+| awk '{ c=$1; $1=""; sub(/^[[:space:]]*/, ""); print c "," $0; count++ } END { print "," count+0 }' \
+>> data_analysis/research_themes.csv
+
+cut -d"," -f2 < data_analysis/research_themes.csv \
+| awk '{ print $0}'
 echo "Subtask 1 was completed"
 echo "======================================"
 
